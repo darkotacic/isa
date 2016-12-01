624c4cdfd56e15e135b20eec101c43d69dacbecc
Darko Tacic <darkotacic@gmail.com>
2016-12-01 19:28:23 +0100

    Pokusaj

---
+++ isaProjekat/src/main/java/com/isa/IsaProjekatApplication.java
@@ -0,0 +1,12 @@
+package com.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaProjekatApplication.class, args);
+	}
+}
