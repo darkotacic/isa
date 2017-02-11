39a44b562782265f6b391f7c131a33993b828d5a
Darko Tacic <darko.tacic@gmail.com>
2017-02-11 14:21:46 +0100

    Dodat entitet BidderOffer.

--- isaProjekat/src/main/java/com/isa/entity/BidderOffer.java
+++ isaProjekat/src/main/java/com/isa/entity/BidderOffer.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.isa.entity.users.Bidder;
 
+@Entity
+@Table(name="BIDDER_OFFER")
 public class BidderOffer implements Serializable {
 
 	/**
--- isaProjekat/src/main/java/com/isa/repository/SystemManagerRepository.java
+++ isaProjekat/src/main/java/com/isa/repository/SystemManagerRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.isa.entity.users.SystemManager;
 
 public interface SystemManagerRepository extends CrudRepository<SystemManager, Long> {
+	
 	@SuppressWarnings("unchecked")
 	SystemManager save(SystemManager sm);
+	
+	SystemManager findByEmail(String email);
+	
 }
