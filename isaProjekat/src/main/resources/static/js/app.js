9623082e2b137796663652943ea74cef6a8fa1e2
CaricaMilca <caricamilca@gmail.com>
2017-02-21 15:40:36 +0100

    Rutiranje. :D

--- isaProjekat/src/main/resources/static/js/app.js
+++ isaProjekat/src/main/resources/static/js/app.js
@@ -9,9 +9,41 @@ app.config(function($routeProvider , $locationProvider){
 			templateUrl: 'login.html',
 			controller: 'loginController'
 		})
-		.when('/register', {
-			templateUrl: 'register.html',
-			controller: 'registerController'
+		.when('/registerRestaurant', {
+			templateUrl: 'registerRestaurant.html',
+			controller: 'systemManagerController'
+		})
+		.when('/showRestaurants', {
+			templateUrl: 'restaurants.html',
+			controller: 'systemManagerController'
+		})
+		.when('/showSystemManagers', {
+			templateUrl: 'systemManagers.html',
+			controller: 'systemManagerController'
+		})
+		.when('/showSystemManagerProfile', {
+			templateUrl: 'editProfileSystemManager.html',
+			controller: 'systemManagerController'
+		})
+		.when('/showActiveRequestOffers', {
+			templateUrl: 'activeRequestOffers.html',
+			controller: 'bidderController'
+		})
+		.when('/showBidderOffersForBidder', {
+			templateUrl: 'bidderOffersForBidder.html',
+			controller: 'bidderController'
+		})
+		.when('/showBidderProfile', {
+			templateUrl: 'editProfileBidder.html',
+			controller: 'bidderController'
+		})
+		.when('/showRestaurantForManager', {
+			templateUrl: 'restaurantForManager.html',
+			controller: 'restaurantManagerController'
+		})
+			.when('/showRestaurantWorkers', {
+			templateUrl: 'workersOfRestaurant.html',
+			controller: 'restaurantManagerController'
 		})
 });
 
